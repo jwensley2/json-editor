24357c333f2269aaa884e3c822a92f02f83544d3
Joseph Wensley <jwensley@intimateinteractive.com>
2018-04-17 15:13:05 -0400

    Add Bootstrap 4 and Font Awesome 5 support

---
+++ src/iconlibs/fontawesome5.js
@@ -0,0 +1,14 @@
+JSONEditor.defaults.iconlibs.fontawesome5 = JSONEditor.AbstractIconLib.extend({
+  mapping: {
+    collapse: 'caret-square-down far',
+    expand: 'caret-square-right far',
+    "delete": 'trash-alt far',
+    edit: 'pencil far',
+    add: 'plus fa',
+    cancel: 'ban fa',
+    save: 'save far',
+    moveup: 'arrow-up fa',
+    movedown: 'arrow-down fa'
+  },
+  icon_prefix: 'fa-'
+});
